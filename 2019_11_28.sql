1532ba40cf069558829684f3839f8e93029ccb41
jwlee <jwlee.lecture@gmail.com>
2019-11-28 12:46:59 +0900

    JOIN 과정 스프레드쉬트 정리 JOIN  NATURAL JOIN  JOIN USING  JOIN ON     SELF JOIN     non-equi join
    JOIN 실습
     . 직원 이름, 직원의 관리자 이름,
       직원의 관리자의 관리자 이름,
       직원의 관리자의 관리자의 관리자 이름
    
    JOIN 실습(JOIN0~0_2)

---
+++ 2019_11_28.sql
@@ -0,0 +1,115 @@
+--emp 테이블, dept 테이블 조인
+--4
+SELECT *
+FROM  dept;
+
+UPDATE dept SET dname = 'MARKET SALES'
+WHERE deptno != '30';
+
+SELECT ename, emp.deptno, dept.dname
+FROM emp, dept
+WHERE emp.deptno = dept.deptno
+AND emp.deptno=30;
+
+SELECT ename, deptno
+FROM emp;
+
+SELECT deptno, dname
+FROM dept;
+
+
+--natural join : 조인 테이블간 같은 타입, 같은이름의 컬럼으로
+--               같은 값을 갖을 경우 조인
+DESC emp;
+DESC dept;
+
+--ANSI SQL
+SELECT deptno, a.empno, ename
+FROM emp a NATURAL JOIN dept b;
+
+--oracle 문법
+SELECT a.deptno, empno, ename
+FROM emp a, dept b
+WHERE a.deptno = b.deptno;
+
+--JOIN USING 
+--join 하려고하는 테이블간 동일한 이름의 컬럼이 두개 이상일 때
+--join 컬럼을 하나만 사용하고 싶을 때
+
+--ANSI SQL
+SELECT *
+FROM emp JOIN dept USING (deptno);
+
+--ORACLE SQL
+SELECT *
+FROM emp, dept
+WHERE emp.deptno = dept.deptno;
+
+--ANSI JOIN with ON
+--조인 하고자 하는 테이블의 컬럼 이름이 다를 때
+--개발자가 조인 조건을 직접 제어할 때
+
+SELECT *
+FROM emp JOIN dept ON (emp.deptno = dept.deptno);
+
+--oracle
+SELECT *
+FROM emp, dept
+WHERE emp.deptno = dept.deptno;
+
+--SELF JOIN : 같은 테이블간 조인
+--emp 테이블간 조인 할만한 사항 : 직원의 관리자 정보 조회
+--직원의 관리자 정보를 조회
+--직원이름, 관리자이름
+
+--ANSI
+--직원 이름, 직원의 상급자 이름, 직원의 상급자의 상급자 이름
+SELECT e.ename, m.ename
+FROM emp e JOIN emp m ON(e.mgr = m.empno);
+
+MILLER  CLARK  KING
+
+--ORACLE
+-- 직원이름, 직원의 관리자 이름, 직원의 관리자의 관리자 이름,
+-- 직원의 관리자의 관리자의 관리자이름
+SELECT e.ename, m.ename, t.ename, k.ename
+FROM emp e, emp m, emp t, emp k
+WHERE e.mgr = m.empno
+AND m.mgr = t.empno
+AND t.mgr = k.empno;
+
+--여러테이블을 ANSI JOIN을 이용한 JOIN
+SELECT e.ename, m.ename, t.ename, k.ename
+FROM emp e JOIN emp m ON ( e.mgr = m.empno)
+     JOIN emp t ON ( m.mgr = t.empno)
+     JOIN emp k ON ( t.mgr = k.empno);
+
+
+--직원의 이름과, 해당 직원의 관리자 이름을 조회한다
+--단 직원의 사번이 7369~7698인 직원을 대상으로 조회
+
+
+
+
+
+
+
+
+
+KING
+    CLARK
+        MILLER
+    BLAKE
+        TURNER
+        JAMES
+        ALLEN
+        WARD
+        MARTIN
+    JONES
+        FORD
+            SMITH
+        SCOTT
+            ADMAS
+
+
+
