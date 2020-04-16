1433c90393597831d4aeaa7b19e989d8195de1eb
jwlee <jwlee.lecture@gmail.com>
2020-04-16 17:08:41 +0900

    20200414

---
+++ 20200414.sql
@@ -0,0 +1,15 @@
+/*SQL 주석*/
+--한줄 주석  --> JAVA의 한줄 주석 //
+/* 여러줄 주석*/
+
+SELECT *        -- 모든 컬럼의 정보를 조회
+FROM lprod;
+
+SELECT *        -- 모든 컬럼의 정보를 조회
+FROM prod;      -- prod 테이블에서
+
+SQL 실행하는 방법
+1. 실행하려는 SQL을 선택하고 (shift) cntl + enter;
+2. 실행하려는 SQL문장 아무곳에서나 프롬프트를 위치시키고 cntl + enter;
+   * 단 위아래 다른 sql 문장이 있을 경우 ;에 의해 sql이 구분되지 못할경우 실행되지 못함
+
