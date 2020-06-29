59377a21a068dcbe025e1eefb52f50bdb0ce1462
jwlee <jwlee.lecture@gmail.com>
2020-06-29 12:11:21 +0900

    first commit

---
+++ 20200624.sql
@@ -0,0 +1,21 @@
+사용자 생성 및 테이블 생성
+user_create.sql
+scott.sql
+base_table.sql
+users.sql
+
+SELECT 실습
+문법
+SELECT * | {column1, column2....}
+FROM 테이블명;
+
+SELECT *
+FROM prod;
+
+SELECT prod_id, prod_name
+FROM prod;
+
+
+
+
+
